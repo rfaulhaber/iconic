a834ee149a3fb9f5d7a7e0792701a0742234063b
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-08-14 20:01:25 -0400

    very basic functionality implemented

---
+++ README.md
@@ -0,0 +1,4 @@
+# iconic
+
+Simple CLI tool for generating image hashes, based on the blockies JS library. 
+A work in progress.
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
 extern crate clap;
 extern crate image;
+extern crate imageproc;
 extern crate rand;
 extern crate rand_xorshift;
 use clap::{App, Arg};
-use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
+use image::{GenericImage, ImageFormat, Pixel, Rgb, RgbImage};
+use imageproc::drawing::draw_filled_rect_mut;
+use imageproc::rect::{Rect, RectPosition};
 use rand::prelude::*;
 use rand::SeedableRng;
 use rand_xorshift::*;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use std::path::Path;
 
-struct Options {
-    size: u16,
-    seed: String,
-    monochrome: bool,
-    symmetric: bool,
+enum Format {
+    JPG,
+    PNG,
+}
+
+impl Format {
+    fn from_str(s: &str) -> Option<Format> {
+        match s {
+            "jpg" => Some(Format::JPG),
+            "png" => Some(Format::PNG),
+            _ => None,
+        }
+    }
+
+    fn to_str(&self) -> &str {
+        match self {
+            Format::JPG => "jpg",
+            Format::PNG => "png",
+        }
+    }
 }
 
 fn main() {
     let app = App::new("iconic")
-        .version("0.1")
+        .version("0.1.0")
         .about("CLI tool for generating icons based on input")
         .author("Ryan Faulhaber <faulhaberryan@gmail.com>")
         .arg(
@@ -28,13 +47,6 @@ fn main() {
                 .takes_value(true)
                 .help("Size of image, s x s.")
                 .default_value("50"),
-        )
-        .arg(
-            Arg::with_name("scale")
-                .long("scale")
-                .takes_value(true)
-                .help("Scale of image")
-                .default_value("10"),
         )
         .arg(
             Arg::with_name("monochrome")
@@ -48,11 +60,27 @@ fn main() {
                 .long("symmetric")
                 .help("Whether or not avatar is symmetric"),
         )
+        .arg(
+            Arg::with_name("out")
+                .short("o")
+                .long("out")
+                .default_value(".")
+                .help("Directory to output"),
+        )
+        .arg(
+            Arg::with_name("format")
+                .short("f")
+                .long("format")
+                .default_value("png")
+                .help("Image format to output. Possible values are: [png, jpg]"),
+        )
         .arg(Arg::with_name("input").help("Value to hash"));
 
     let matches = app.get_matches();
 
     let input = matches.value_of("input").unwrap();
+    let path = Path::new(matches.value_of("out").unwrap());
+    let format = matches.value_of("format").unwrap();
 
     let mut hasher = DefaultHasher::new();
 
@@ -61,26 +89,37 @@ fn main() {
     let mut rng = XorShiftRng::seed_from_u64(hasher.finish());
 
     let size = matches.value_of("size").unwrap().parse::<u32>().unwrap();
-    let scale = matches.value_of("scale").unwrap().parse::<u32>().unwrap();
 
-    let mut imgbuf: RgbImage = ImageBuffer::new(size * scale, size * scale);
+    let mut img = RgbImage::new(size, size);
 
     let primary = [rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
     let secondary = [rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
     let background = [rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
 
-    let width = rng.gen_range(10, (size * scale) / 2);
+    for _ in 0..rng.gen_range(5, size / 3) {
+        let (x, y) = (
+            rng.gen_range(-(size as i32), size as i32),
+            rng.gen_range(-(size as i32), size as i32),
+        );
+        let (width, height) = (rng.gen_range(10, size / 2), rng.gen_range(10, size / 2));
+        let rect = Rect::at(x, y).of_size(width, height);
 
-    let run = rng.gen_ratio(1, 2);
-    for (x, pixel) in imgbuf.enumerate_rows_mut() {
-        // if run {
-        //     pixel = image::Rgb(primary);
-        // } else {
-        //     pixel = image::Rgb(secondary);
-        // }
+        let flip = rng.gen_ratio(1, 3);
+
+        if flip {
+            draw_filled_rect_mut(&mut img, rect, image::Rgb(primary));
+        } else {
+            draw_filled_rect_mut(&mut img, rect, image::Rgb(secondary));
+        }
     }
 
-    imgbuf
-        .save("/Users/ryanfaulhaber/Desktop/test.png")
-        .unwrap();
+    for (_, _, pixel) in img.enumerate_pixels_mut() {
+        if pixel.to_rgb() == image::Rgb([0, 0, 0]) {
+            *pixel = image::Rgb(background);
+        }
+    }
+
+    let name = format!("{}/{}-{}.{}", path.display(), input, size, format);
+
+    img.save(name).unwrap();
 }
